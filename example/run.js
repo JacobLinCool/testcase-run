3e49c92711914222b395f10074a2a653543f8b3e
JacobLinCool <jacoblincool@gmail.com>
2021-10-30 01:01:49 +0800

    chore(example): add example

---
+++ example/run.js
@@ -0,0 +1,11 @@
+const { Runner } = require("../lib");
+const fs = require("fs");
+const path = require("path");
+
+const testcases = fs.readFileSync(path.join(__dirname, "testcases.json"), "utf-8");
+const runner = new Runner(testcases);
+
+runner.run("D:\\JacobLinCool\\Github\\c-learning\\hw2\\hw0203.exe", { timeout: 10_000 }).then((report) => {
+    fs.writeFileSync(path.join(__dirname, "report.json"), JSON.stringify(report, null, 2), "utf-8");
+    console.log(`runned ${report.reduce((acc, cur) => acc + cur.results.length, 0)} testcases`);
+});
