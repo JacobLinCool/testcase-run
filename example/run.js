28220c35c0af03ba839eafffde7d491301612bde
JacobLinCool <jacoblincool@gmail.com>
2021-10-30 15:48:40 +0800

    chore(example): add all-run time

--- example/run.js
+++ example/run.js
@@ -5,7 +5,8 @@ const path = require("path");
 const testcases = fs.readFileSync(path.join(__dirname, "testcases.json"), "utf-8");
 const runner = new Runner(testcases);
 
+const StartT = Date.now();
 runner.run("D:\\JacobLinCool\\Github\\c-learning\\hw2\\hw0203.exe", { timeout: 10_000 }).then((report) => {
     fs.writeFileSync(path.join(__dirname, "report.json"), JSON.stringify(report, null, 2), "utf-8");
-    console.log(`runned ${report.reduce((acc, cur) => acc + cur.results.length, 0)} testcases`);
+    console.log(`runned ${report.reduce((acc, cur) => acc + cur.results.length, 0)} testcases in ${Date.now() - StartT}ms`);
 });
